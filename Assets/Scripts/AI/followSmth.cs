0e2cb23ac8512751be30a63b1a301c622759f0a5
Sarah Schütz <sarahschuetz@gmx.at>
2015-09-18 17:02:55 +0300

    snow is falling now :)

--- Assets/Scripts/AI/followSmth.cs
+++ Assets/Scripts/AI/followSmth.cs
@@ -9,6 +9,11 @@ public class followSmth : MonoBehaviour {
 	// How fast does the follower turns around
 	public float rotationSpeed = 10f;
 	
+	// Should the follower be at the same position
+	public bool samePosition = false;
+	
+	public Vector3 offset = new Vector3(0, 0, 0);
+	
 	// Distance the follower starts following
 	public int followDistance = 100;
 	
@@ -26,22 +31,28 @@ public class followSmth : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		this.distance = Vector3.Distance(target.transform.position, transform.position);
-		
-		if(this.distance <= this.followDistance) {
+		if(this.samePosition) {
 			
-			// rotate follower
-			Vector3 followerDirection = transform.position - target.transform.position;
+			transform.position = target.transform.position + offset;
+		}
+		else {
+			this.distance = Vector3.Distance(target.transform.position, transform.position);
 			
-			float rotateStep = speed * Time.deltaTime;
-			
-			Vector3 newDirection = Vector3.RotateTowards(transform.forward, followerDirection, rotateStep, 0.0F);
-			Debug.DrawRay(transform.position, newDirection, Color.red);
-        	transform.rotation = Quaternion.LookRotation(newDirection);
-			
-			// move follower
-			float step = speed * Time.deltaTime;
-		    transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+			if(this.distance <= this.followDistance) {
+				
+				// rotate follower
+				Vector3 followerDirection = transform.position - target.transform.position;
+				
+				float rotateStep = speed * Time.deltaTime;
+				
+				Vector3 newDirection = Vector3.RotateTowards(transform.forward, followerDirection, rotateStep, 0.0F);
+				Debug.DrawRay(transform.position, newDirection, Color.red);
+				transform.rotation = Quaternion.LookRotation(newDirection);
+				
+				// move follower
+				float step = speed * Time.deltaTime;
+				transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+			}
 		}
 	}
 }
