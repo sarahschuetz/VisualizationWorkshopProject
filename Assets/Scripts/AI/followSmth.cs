7fa93af83fad3040e97827456de109bab92c16e2
Sarah Schütz <sarahschuetz@gmx.at>
2015-09-18 12:40:13 +0300

    edit followSmth script and add collision of bear and player

---
+++ Assets/Scripts/AI/followSmth.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followSmth : MonoBehaviour {
+
+	// How fast does the follower run after you
+	public float speed = 10f;
+	
+	// How fast does the follower turns around
+	public float rotationSpeed = 10f;
+	
+	// Distance the follower starts following
+	public int followDistance = 100;
+	
+	// Object which the current object will followDistance
+	public Rigidbody target;
+	
+	// What is the distance between you and your follower
+	private float distance;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.distance = Vector3.Distance(target.transform.position, transform.position);
+		
+		if(this.distance <= this.followDistance) {
+			
+			// rotate follower
+			Vector3 followerDirection = transform.position - target.transform.position;
+			
+			float rotateStep = speed * Time.deltaTime;
+			
+			Vector3 newDirection = Vector3.RotateTowards(transform.forward, followerDirection, rotateStep, 0.0F);
+			Debug.DrawRay(transform.position, newDirection, Color.red);
+        	transform.rotation = Quaternion.LookRotation(newDirection);
+			
+			// move follower
+			float step = speed * Time.deltaTime;
+		    transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+		}
+	}
+}
---
+++ Assets/Scripts/AI/hitPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hitPlayer : MonoBehaviour {
+	
+	public Rigidbody player;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter(Collision hit) {
+		if (hit.gameObject.name == player.name){
+			print("YOU'R DONE");
+		}
+	}
+}
